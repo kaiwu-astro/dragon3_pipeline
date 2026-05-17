kaiwu-astro/dragon3_pipeline

commit 751f3ac1c358dfdec3d6f51088bbe0c405034ab8

fix: confusing underscore removal in limit dict key; config default to skip until last

dragon3_pipelines/config/settings.py
@@ -114,18 +114,6 @@ class ConfigManager:
         # Binary types
         self.wow_binary_st_list: List[str] = config["wow_binary_st_list"]
 
-        # Limits
-        self.limits: Dict[str, Tuple[float, float]] = {
-            k.replace("_", " ").replace("[", " [").replace("]", "]"): tuple(v)
-            for k, v in config["limits"].items()
-        }
-
-        # Column labels
-        self.colname_to_label: Dict[str, str] = {
-            k.replace("_", " ").replace("[", " [").replace("]", "]"): v
-            for k, v in config["column_labels"].items()
-        }
-
         # Fixed width font context
         self.fixed_width_font_context: Dict[str, Any] = {"rc": {"font.family": "monospace"}}
 

dragon3_pipelines/config/default_config.yaml
@@ -6,16 +6,16 @@ paths:
     "0sb": "/p/scratch/madnuc/wu13/mar17-dragon3-start/00softBinary-5hardBinary"
     "20sb": "/p/scratch/madnuc/wu13/mar17-dragon3-start/20softBinary-5hardBinary"
     "60sb": "/p/scratch/madnuc/wu13/mar17-dragon3-start/60softBinary-5hardBinary"
-  
+
   # Input file paths
   input_files:
     "0sb": "N1m-Dragon3.inp.init"
     "20sb": "N1m-Dragon3.inp.init"
     "60sb": "N1m-Dragon3.inp.init"
-  
+
   # Plot directory
   plot_dir: "/p/home/jusers/wu13/juwels/scratch/plot"
-  
+
   # Cache directory suffix
   cache_dir_suffix: "/particle_df_cache"
 
@@ -28,9 +28,9 @@ figure_prefixes:
 # Processing options
 processing:
   skip_until:
-    "0sb": 700
-    "20sb": 60
-    "60sb": 0
+    "0sb": "last"
+    "20sb": "last"
+    "60sb": "last"
   skip_existing_plot: true
   plot_only_int_nbody_time: true
   close_figure_in_ipython: false

commit 49cd1d39442746cd85bccc4c017e2e14def96531

fix problems introduced in 751f3ac

dragon3_pipelines/config/settings.py
@@ -114,6 +114,12 @@ class ConfigManager:
         # Binary types
         self.wow_binary_st_list: List[str] = config["wow_binary_st_list"]
 
+        # Limits
+        self.limits: Dict[str, Tuple[float, float]] = config["limits"]
+
+        # Column labels
+        self.colname_to_label: Dict[str, str] = config["column_labels"]
+
         # Fixed width font context
         self.fixed_width_font_context: Dict[str, Any] = {"rc": {"font.family": "monospace"}}
 